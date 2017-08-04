5700979261db0b0b0c2277767612947f15011d6e
lgh <matiaslgh@gmail.com>
2017-08-03 23:22:38 -0300

    style: improve indentation consistency by replacing spaces by tabs

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import SearchPage from './SearchPage'
 import MainPage from './MainPage'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
+	state = {
+		/**
+		 * TODO: Instead of using this state variable to keep track of which page
+		 * we're on, use the URL in the browser's address bar. This will ensure that
+		 * users can use the browser's back and forward buttons to navigate between
+		 * pages, as well as provide a good URL they can bookmark and share.
+		 */
+		showSearchPage: true
+	}
 
-  render() {
-    return (
-      <div className="app">
-        <Route exact path="/" component={MainPage} />
+	render() {
+		return (
+			<div className="app">
+				<Route exact path="/" component={MainPage} />
 
-        <Route exact path="/search" component={SearchPage} />
-      </div>
-    )
-  }
+				<Route exact path="/search" component={SearchPage} />
+			</div>
+		)
+	}
 }
 
 export default BooksApp
