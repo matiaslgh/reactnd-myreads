de20eeb876391543af2398dd0929522978dc8db5
lgh <matiaslgh@gmail.com>
2017-08-05 20:55:21 -0300

    feat: search via API

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import debounce from 'lodash.debounce'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class SearchPage extends Component {
+
+	state = {
+		results: []
+	}
+
+	search = debounce(query => {
+		if (!query) {
+			this.setState({results: []})
+			return
+		}
+
+		BooksAPI.search(query).then(results => {
+			this.setState(() => results && !results.error ? {results} : {results: []})
+		})
+	}, 300)
+
 	render() {
 		return (
 			<div className="search-books">
 				<div className="search-books-bar">
 					<Link className="close-search" to="/">Close</Link>
 					<div className="search-books-input-wrapper">
-						{/*
-							NOTES: The search from BooksAPI is limited to a particular set of search terms.
-							You can find these search terms here:
-							https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-							However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-							you don't find a specific author or title. Every search is limited by search terms.
-						*/}
-						<input type="text" placeholder="Search by title or author"/>
-
+						<input type="text" placeholder="Search by title or author" onChange={e => {this.search(e.target.value)}}/>
 					</div>
 				</div>
 				<div className="search-books-results">
-					<ol className="books-grid"></ol>
+					<ol className="books-grid">
+						{this.state.results.map(book => (
+							<li key={book.id}><Book book={book} /></li>
+						))}
+					</ol>
 				</div>
 			</div>
 		)
